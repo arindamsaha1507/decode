54c01b17095228aec2a83d959438b4ba7773dd18
Arindam Saha <arindamsaha1507@gmail.com>
2023-11-21 06:21:29 -0500

    Prompt now composed

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 """The main file for the project."""
 
-import decode.ai_handler as ai_handler
+from decode.ai_handler import AIHandler
+from decode.file_handler import FileHandler
+from decode.string_constructor import StringConstructor
 
 
 def main():
     """The main function for the project."""
-    handler = ai_handler.AIHandler(
+
+    header = FileHandler.read_file_contents("prompt_header.txt")
+    equation = FileHandler.read_file_contents("equation.txt")
+    footer = FileHandler.read_file_contents("prompt_footer.txt")
+
+    prompt = StringConstructor.construct_string_contents(header, equation, footer)
+
+    FileHandler.write_file_contents("prompt.txt", prompt)
+
+    handler = AIHandler(
         settings_file_path="settings.txt",
         prompt_file_path="prompt.txt",
     )
